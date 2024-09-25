afa4ffa185c097eee42605e51432ebf94d87e10f
Hussam <hosamafiky@gmail.com>
2024-09-25 16:52:31 +0300

    code changes

--- lib/features/html/screens/html_viewer_screen.dart
+++ lib/features/html/screens/html_viewer_screen.dart
@@ -32,29 +32,31 @@ class _HtmlViewerScreenState extends State<HtmlViewerScreen> {
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: CustomAppBar(
-          title: widget.htmlType == HtmlType.termsAndCondition
-              ? 'terms_conditions'.tr
-              : widget.htmlType == HtmlType.aboutUs
-                  ? 'about_us'.tr
-                  : widget.htmlType == HtmlType.privacyPolicy
-                      ? 'privacy_policy'.tr
-                      : widget.htmlType == HtmlType.shippingPolicy
-                          ? 'shipping_policy'.tr
-                          : widget.htmlType == HtmlType.refund
-                              ? 'refund_policy'.tr
-                              : widget.htmlType == HtmlType.cancellation
-                                  ? 'cancellation_policy'.tr
-                                  : 'no_data_found'.tr),
+        title: widget.htmlType == HtmlType.termsAndCondition
+            ? 'terms_conditions'.tr
+            : widget.htmlType == HtmlType.aboutUs
+                ? 'about_us'.tr
+                : widget.htmlType == HtmlType.privacyPolicy
+                    ? 'privacy_policy'.tr
+                    : widget.htmlType == HtmlType.shippingPolicy
+                        ? 'shipping_policy'.tr
+                        : widget.htmlType == HtmlType.refund
+                            ? 'refund_policy'.tr
+                            : widget.htmlType == HtmlType.cancellation
+                                ? 'cancellation_policy'.tr
+                                : 'no_data_found'.tr,
+      ),
       endDrawer: const MenuDrawer(),
       endDrawerEnableOpenDragGesture: false,
-      body: GetBuilder<HtmlController>(builder: (htmlController) {
-        return Center(
-          child: htmlController.htmlText != null
-              ? SingleChildScrollView(
-                  physics: const BouncingScrollPhysics(),
-                  child: Column(
-                    children: [
-                      WebScreenTitleWidget(
+      body: GetBuilder<HtmlController>(
+        builder: (htmlController) {
+          return Center(
+            child: htmlController.htmlText != null
+                ? SingleChildScrollView(
+                    physics: const BouncingScrollPhysics(),
+                    child: Column(
+                      children: [
+                        WebScreenTitleWidget(
                           title: widget.htmlType == HtmlType.termsAndCondition
                               ? 'terms_conditions'.tr
                               : widget.htmlType == HtmlType.aboutUs
@@ -67,48 +69,54 @@ class _HtmlViewerScreenState extends State<HtmlViewerScreen> {
                                               ? 'refund_policy'.tr
                                               : widget.htmlType == HtmlType.cancellation
                                                   ? 'cancellation_policy'.tr
-                                                  : 'no_data_found'.tr),
-                      FooterView(
+                                                  : 'no_data_found'.tr,
+                        ),
+                        FooterView(
                           child: Ink(
-                        width: Dimensions.webMaxWidth,
-                        color: Theme.of(context).cardColor,
-                        padding: const EdgeInsets.all(Dimensions.paddingSizeSmall),
-                        child: Column(mainAxisAlignment: MainAxisAlignment.start, children: [
-                          // (htmlController.htmlText!.contains('<ol>') || htmlController.htmlText!.contains('<ul>')) ? HtmlWidget(
-                          //   htmlController.htmlText ?? '',
-                          //   key: Key(widget.htmlType.toString()),
-                          //   onTapUrl: (String url) {
-                          //     return launchUrlString(url, mode: LaunchMode.externalApplication);
-                          //   },
-                          // ) : SelectableHtml(
-                          //   data: htmlController.htmlText, shrinkWrap: true,
-                          //   onLinkTap: (String? url, RenderContext context, Map<String, String> attributes, element) {
-                          //     if(url!.startsWith('www.')) {
-                          //       url = 'https://$url';
-                          //     }
-                          //     if (kDebugMode) {
-                          //       log('Redirect to url: $url');
-                          //     }
-                          //     html.window.open(url, "_blank");
-                          //   },
-                          // ),
+                            width: Dimensions.webMaxWidth,
+                            color: Theme.of(context).cardColor,
+                            padding: const EdgeInsets.all(Dimensions.paddingSizeSmall),
+                            child: Column(
+                              mainAxisAlignment: MainAxisAlignment.start,
+                              children: [
+                                // (htmlController.htmlText!.contains('<ol>') || htmlController.htmlText!.contains('<ul>')) ? HtmlWidget(
+                                //   htmlController.htmlText ?? '',
+                                //   key: Key(widget.htmlType.toString()),
+                                //   onTapUrl: (String url) {
+                                //     return launchUrlString(url, mode: LaunchMode.externalApplication);
+                                //   },
+                                // ) : SelectableHtml(
+                                //   data: htmlController.htmlText, shrinkWrap: true,
+                                //   onLinkTap: (String? url, RenderContext context, Map<String, String> attributes, element) {
+                                //     if(url!.startsWith('www.')) {
+                                //       url = 'https://$url';
+                                //     }
+                                //     if (kDebugMode) {
+                                //       log('Redirect to url: $url');
+                                //     }
+                                //     html.window.open(url, "_blank");
+                                //   },
+                                // ),
 
-                          HtmlWidget(
-                            htmlController.htmlText ?? '',
-                            key: Key(widget.htmlType.toString()),
-                            textStyle: robotoRegular.copyWith(color: Theme.of(context).textTheme.bodyLarge?.color?.withOpacity(0.6)),
-                            onTapUrl: (String url) {
-                              return launchUrlString(url);
-                            },
+                                HtmlWidget(
+                                  htmlController.htmlText ?? '',
+                                  key: Key(widget.htmlType.toString()),
+                                  textStyle: robotoRegular.copyWith(color: Theme.of(context).textTheme.bodyLarge?.color?.withOpacity(0.6)),
+                                  onTapUrl: (String url) {
+                                    return launchUrlString(url);
+                                  },
+                                ),
+                              ],
+                            ),
                           ),
-                        ]),
-                      ))
-                    ],
-                  ),
-                )
-              : const CircularProgressIndicator(),
-        );
-      }),
+                        ),
+                      ],
+                    ),
+                  )
+                : const CircularProgressIndicator(),
+          );
+        },
+      ),
     );
   }
 }
